ae93308ab1a718ed0cad4df85e8949013d91dc92
Alexander Kornijenko <alexander.kornijenko@googlemail.com>
2012-09-03 19:55:30 +0200

    minor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120818154335) do
+ActiveRecord::Schema.define(:version => 20120830115517) do
 
   create_table "categories", :force => true do |t|
     t.string   "cname"
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(:version => 20120818154335) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "todo_id"
+  end
+
+  create_table "searches", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "todos", :force => true do |t|
