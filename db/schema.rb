382ddb7fd44fb911414092d5ee6cc7c63e76f0b9
Alexander Kornijenko <alexander.kornijenko@googlemail.com>
2012-09-04 18:06:12 +0200

    Profilfoto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120904020115) do
+ActiveRecord::Schema.define(:version => 20120904125548) do
 
   create_table "categories", :force => true do |t|
     t.string   "cname"
@@ -72,10 +72,14 @@ ActiveRecord::Schema.define(:version => 20120904020115) do
     t.date     "born"
     t.string   "city"
     t.boolean  "enabled"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
